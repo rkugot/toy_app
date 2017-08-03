e3fb73a18e95e8acd720663cb1b49ef1da3b99fd
rkugot <stream13k@gmail.com>
2017-08-03 15:02:56 +0300

    Add length validation to micropost model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root "users#index"
-  resources :users do
-    resources :microposts
-  end
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
